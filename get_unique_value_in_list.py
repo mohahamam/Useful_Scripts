c88377f2d3fae367db8cbe2cb6f4fd2c3e9e28d7
Mohamed Hamam <mohamed.hamam@gmail.com>
2018-12-29 21:53:04 +0400

    extracting a unique PONs list from ONTs list

--- get_unique_value_in_list.py
+++ get_unique_value_in_list.py
@@ -1,3 +1,4 @@
+import re
 listofONTs = ['1/1/1/1/1','1/1/1/1/1','1/1/1/2/1','1/1/3/4/1','1/1/3/5/16','1/1/1/1/1','1/1/3/4/1','1/1/13/14/11','1/1/13/14/11']
 print (listofONTs)
 
@@ -9,3 +10,20 @@ print(setofONTs[1])
 print(setofONTs[2])
 
 print(len(setofONTs))
+print(type(setofONTs))
+
+setofONTs1 = sorted({n.replace('/','-') for n in listofONTs if n!='1/1/1/1/1' if n!='1/1/1/2/1'})
+print(setofONTs1)
+
+PONRegex = r'(\b1/1/(1[0-6]|[1-9])/(1[0-6]|[1-9])\b)'
+#test = re.findall(PONRegex,'1/1/15/16/1')
+#print(test[0][0])
+
+def findPON(ONTnumber):
+	PONnumber = re.findall(PONRegex,ONTnumber)
+	return PONnumber
+
+listofPONs =sorted({findPON(n)[0][0] for n in listofONTs})
+print(listofPONs)
+
+
